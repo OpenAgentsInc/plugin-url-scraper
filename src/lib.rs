6e75ff359a464f0de620239de3ce74dfe1a28df2
Christopher David <chris@arcadelabs.co>
2024-02-01 10:00:56 -0600

    Initial extraction works

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 use extism_pdk::*;
+use regex::Regex;
+use serde_json::to_string;
 
 #[plugin_fn]
-pub fn greet(name: String) -> FnResult<String> {
-    Ok(format!("Hello, {}!", name))
+pub fn extract_urls(input: String) -> FnResult<String> {
+    let url_regex = Regex::new(r"https?://[^\s]+").unwrap();
+    let urls: Vec<String> = url_regex.find_iter(&input)
+                                    .map(|mat| mat.as_str().to_string())
+                                    .collect();
+
+    let serialized_urls = to_string(&urls).unwrap();
+    Ok(serialized_urls)
 }
+
